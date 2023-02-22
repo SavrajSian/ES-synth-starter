d55190ce86cb73fae888fdc28cd8c1c6496ee403
Savraj Sian <ss2320@ic.ac.uk>
2023-02-22 13:08:40 +0000

    progress

--- src/main.cpp
+++ src/main.cpp
@@ -79,6 +79,73 @@ void setup() {
   Serial.println("Hello World");
 }
 
+u_int8_t readCols() {
+
+  bool a = digitalRead(C0_PIN);
+  bool b = digitalRead(C1_PIN);
+  bool c = digitalRead(C2_PIN);
+  bool d = digitalRead(C3_PIN);
+
+  u_int8_t keys =  d<<3 | c<<2 | b<<1 | a;
+  return keys;
+}
+
+void setRow(uint8_t rowIdx){
+  digitalWrite(REN_PIN, HIGH);
+  digitalWrite(RA0_PIN, rowIdx & 0b001);
+  digitalWrite(RA1_PIN, rowIdx & 0b010);
+  digitalWrite(RA2_PIN, rowIdx & 0b100);
+
+}
+
+const int32_t stepSizes [] = { 
+  51076063,54113191,57330941,60740013,64351807,68178349,72232447,76527610,81078186,85899345,91007194,96418755
+};
+
+volatile int32_t currentStepSize;
+
+const std::string keyNames [] = {
+  "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
+};
+
+
+
+std::string getkey(uint8_t keyArray[7]){
+  u_int8_t l = keyArray[0];
+  u_int8_t m = keyArray[1];
+  u_int8_t h = keyArray[2];
+  u_int32_t keys = h <<8 | m << 4 | l;
+
+  switch (keys){
+    case 0b111111111110:
+      return keyNames[0];
+    case 0b111111111101:
+      return keyNames[1];
+    case 0b111111111011:
+      return keyNames[2];
+    case 0b111111110111:
+      return keyNames[3];
+    case 0b111111101111:
+      return keyNames[4];
+    case 0b111111011111:
+      return keyNames[5];
+    case 0b111110111111:
+      return keyNames[6];
+    case 0b111101111111:
+      return keyNames[7];
+    case 0b111011111111:
+      return keyNames[8];
+    case 0b110111111111:
+      return keyNames[9];
+    case 0b101111111111:
+      return keyNames[10];
+    case 0b011111111111:
+      return keyNames[11];
+    default:
+      return "X";
+  }
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
   static uint32_t next = millis();
@@ -90,12 +157,33 @@ void loop() {
     //Update display
     u8g2.clearBuffer();         // clear the internal memory
     u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-    u8g2.drawStr(2,10,"Helllo World!");  // write something to the internal memory
+    u8g2.drawStr(2,10,"Hello World!");  // write something to the internal memory
+
+    //Read key matrix
+    uint8_t keyArray[7];
+    for(int i = 0; i < 3; i++){
+      setRow(i);
+      delayMicroseconds(3);
+      keyArray[i] = readCols();
+    }
+
     u8g2.setCursor(2,20);
-    u8g2.print(count++);
+    u_int8_t keys = keyArray[0]<<2 | keyArray[1] << 1 | keyArray[2];
+    for(int k = 0; k < 8; k++){
+      if (keyArray[k] == 0){
+        currentStepSize = stepSizes[k];
+      }
+    }
+
+    u8g2.print(keys, HEX); 
+    std::string key = getkey(keyArray);
+    if (key != "X") {
+      u8g2.drawStr(2, 30, key.c_str());
+    }
     u8g2.sendBuffer();          // transfer internal memory to the display
 
     //Toggle LED
     digitalToggle(LED_BUILTIN);
+
   }
 }
